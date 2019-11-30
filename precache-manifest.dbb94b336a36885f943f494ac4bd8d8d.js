741c4ceefd56bf6cba2d657230300803322bd7f7
Evan LaHurd <evan.lahurd@gmail.com>
2019-11-29 19:31:39 -0700

    Updates

---
+++ precache-manifest.dbb94b336a36885f943f494ac4bd8d8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba809a0e936c91241bfd4925ecf40f80",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f4d4b3d520ba3cfef14",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1db63eb7e5de2e3803ae",
+    "url": "/robofriends/static/css/main.3eb1d671.chunk.css"
+  },
+  {
+    "revision": "7f4d4b3d520ba3cfef14",
+    "url": "/robofriends/static/js/2.3ff4439a.chunk.js"
+  },
+  {
+    "revision": "1db63eb7e5de2e3803ae",
+    "url": "/robofriends/static/js/main.18402efc.chunk.js"
+  },
+  {
+    "revision": "cb519fe66fcbb61e7a5c",
+    "url": "/robofriends/static/js/runtime-main.0560b89d.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/sega.82e09f78.ttf"
+  }
+]);
